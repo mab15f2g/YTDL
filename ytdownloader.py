e9f4fbe2f5fea8f86edd1dbac87af9ceb80417ee
Marcus <marcus.braeutiga.f2g@fh-zwickau.de>
2020-08-11 15:54:16 +0200

    cut out the canvas elements and refactored code

--- ytdownloader.py
+++ ytdownloader.py
@@ -1,47 +1,28 @@
-# make executable tutorial
-# https://datatofish.com/executable-pyinstaller/
-# https://datatofish.com/entry-box-tkinter/ Textbox
+from tkinter import *
 import tkinter as tk
-from tkinter import Checkbutton, Frame, Scale, HORIZONTAL
 import subprocess
 
+root = tk.Tk()
+root.geometry("600x400")
 
-
-
-main = tk.Tk()
-
-root = Frame(main)
-root.grid(row=0, column=0, sticky="n")
-
-col1 = 150
-col2 = 250
-
-row1 = 50
-row2 = 100
-row3 = 150
-row4 = 200
-row5 = 250
-row6 = 300
-testlink = "https://www.youtube.com/watch?v=2uRO_FabHRo"
-testplaylist = "https://www.youtube.com/playlist?list=PLkdKYxSQmXhOkpa105w1vwPOmv3fBshYv"
-canvas1 = tk.Canvas(root, width=400, height=400)
-canvas1.grid(row=0, column=1)
-
-frame = Frame(root)
-frame.grid(row=0, column=0, sticky="n")
 
 is_checked_playlist = tk.IntVar()
 is_checked_error = tk.IntVar()
- 
-##
+
+
 # Dowload button click methode
-##
 def download():
+    print("________")
+    print(scale.get())
+    
     comm = " "
     comm = comm.join(create_command())
     print(comm)
     # execute dl.exe
     subprocess.call(comm)
+
+
+
 
 # concate the options
 def set_options():
@@ -51,13 +32,14 @@ def set_options():
         options.append("--playlist")
     if is_checked_error.get() == 1:
         options.append("-i")
-    options.append("--audio-quality")
-    options.append(str(scaler.get()))
+    #options.append("--audio-quality")
+    #options.append(str(scale.get()))
     return options
 
 # get link from textfield
 def get_link():
-    link = entry1.get()
+    
+    link = e1.get()
 
     return link
 
@@ -72,55 +54,26 @@ def create_command():
     return command
 
 
-##
 # Labels
-##
+label1 = Label(root, text="YT Link").grid(row=0)
+label2 = Label(root, text="Quality").grid(row=5,column=1, sticky=W)
 
-label3 = tk.Label(text='Playlist?')
-label3.config(font=('helvetica', 8))
-canvas1.create_window(col1, row3, window=label3)
+# Entry fileds 
+e1 = tk.Entry(root, width=50)
+e1.insert(0,"https://www.youtube.com/watch?v=2uRO_FabHRo")
+e1.grid(row=0, column=1)
+# Checkbuttons
+check_playlist = Checkbutton(root, text = "Playlist?" , variable=is_checked_playlist)
+check_playlist.grid(row=1, column=1, sticky=W)
+check_error = Checkbutton(root, text = "Ignore Errors?", variable=is_checked_error )
+check_error.grid(row=2, column=1, sticky=W)
+checkbutton3 = Checkbutton(root, text = "Bypass geoblock?" )
+checkbutton3.grid(row=3, column=1, sticky=W)
 
-label3 = tk.Label(text='Playlist?')
-label3.config(font=('helvetica', 8))
-canvas1.create_window(col1, row2, window=label3)
-
-label3 = tk.Label(text='ignore Errors?')
-label3.config(font=('helvetica', 8))
-canvas1.create_window(col1, row3, window=label3)
-
-label4 = tk.Label(text='Quality?')
-label4.config(font=('helvetica', 8))
-canvas1.create_window(col1, row4, window=label4)
-
-##
-# Buttons
-##
-button1 = tk.Button(text='Download', command=download, bg='brown', fg='white')
-canvas1.create_window(col1, row6, window=button1)
-
-##
-# Checkbox
-##
-#Playlist check
-check_playlist = Checkbutton(root, variable=is_checked_playlist)
-canvas1.create_window(col2, row2, window=check_playlist)
-#Error check
-check_error = Checkbutton(root, variable=is_checked_error)
-canvas1.create_window(col2, row3, window=check_error)
-
-
-##
-# Textfield
-##
-entry1 = tk.Entry(root, width=50) 
-entry1.insert(0, "Paste link here")
-canvas1.create_window(col1 + 25, row1, window=entry1)
-
-##
-# Scaler
-##
-scaler = Scale(root, from_=128, to=320, orient=HORIZONTAL)
-canvas1.create_window(col2, row4, window=scaler)
-
-# main application loop
+# Download button
+button1 = Button(root, text = "Download",command=download, bg='blue', fg='white')
+button1.grid(sticky=E + S)
+# Quality slider
+scale = Scale(root, from_=128, to=320, orient=HORIZONTAL)
+scale.grid(row=5,column=1)
 root.mainloop()
